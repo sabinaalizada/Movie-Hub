a1301b402598339194dd433fbdf929270f3a3482
Sabina Alizada <116658450+sabinaalizada@users.noreply.github.com>
2026-03-13 15:16:05 +0400

    wrote and updated kafka configurations

---
+++ movie-kafka/src/main/java/com/ecommerce/moviekafka/config/KafkaConfig.java
@@ -0,0 +1,40 @@
+package com.ecommerce.moviekafka.config;
+
+import com.ecommerce.moviekafka.event.MovieEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ProducerFactory<String, MovieEvent> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(
+                configProps,
+                new StringSerializer(),
+                new JacksonJsonSerializer<>()
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, MovieEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- movie-kafka/src/main/java/com/ecommerce/moviekafka/producer/KafkaProducer.java
+++ movie-kafka/src/main/java/com/ecommerce/moviekafka/producer/KafkaProducer.java
@@ -2,15 +2,18 @@ package com.ecommerce.moviekafka.producer;
 
 import com.ecommerce.moviekafka.event.MovieEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaProducer {
     private final KafkaTemplate<String, MovieEvent> kafkaTemplate;
 
     public void sendMovieEvent(MovieEvent event) {
-        kafkaTemplate.send("movie.events", event.getMovieId(), event);
+        kafkaTemplate.send("movie.events", event.getId(), event);
+        log.info("Movie event sent");
     }
 }
