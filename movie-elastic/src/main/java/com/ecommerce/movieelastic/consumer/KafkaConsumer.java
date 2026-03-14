83b5c360b3b8aa8cf6a20af3ad0581a5652431af
Sabina Alizada <116658450+sabinaalizada@users.noreply.github.com>
2026-03-14 21:29:35 +0400

    bug fixed

--- movie-elastic/src/main/java/com/ecommerce/movieelastic/consumer/KafkaConsumer.java
+++ movie-elastic/src/main/java/com/ecommerce/movieelastic/consumer/KafkaConsumer.java
@@ -1,6 +1,6 @@
 package com.ecommerce.movieelastic.consumer;
 
-import com.ecommerce.moviekafka.event.MovieEvent;
+import com.ecommerce.moviecore.event.MovieEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
