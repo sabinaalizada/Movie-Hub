8b927175446f8b03f179baca1cca9aa048d30b7d
Sabina Alizada <116658450+sabinaalizada@users.noreply.github.com>
2026-03-14 07:02:01 +0400

    created KafkaConsumer class

--- movie-kafka/src/main/java/com/ecommerce/moviekafka/config/KafkaProducerConfig.java
+++ movie-kafka/src/main/java/com/ecommerce/moviekafka/config/KafkaProducerConfig.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConfig {
+public class KafkaProducerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
---
+++ movie-kafka/src/main/java/com/ecommerce/moviekafka/consumer/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.ecommerce.moviekafka.consumer;
+
+import com.ecommerce.moviekafka.event.MovieEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    @KafkaListener(topics = "movie.events", groupId = "group_Id", containerFactory = "kafkaListenerContainerFactory")
+    public void consumer(MovieEvent message) {
+        log.info("message consumed: {}", message);
+    }
+}
